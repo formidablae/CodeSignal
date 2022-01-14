e6dfafda9bfb347a5ae22a1e22d9e3e0c3e306dd
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-14 18:37:45 +0100

    Solved Arcade / Python / 01 Meet Python / 07 Simple Sort

---
+++ Arcade/Python/01_Meet_Python/07_Simple_Sort/main.py
@@ -0,0 +1,12 @@
+def solution(arr):
+
+    n = len(arr)
+
+    for i in range(n):
+        j = 0
+        stop = n - i
+        while j < stop - 1:
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+            j += 1
+    return arr
