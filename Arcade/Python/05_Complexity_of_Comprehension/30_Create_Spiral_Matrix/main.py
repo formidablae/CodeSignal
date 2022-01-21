9241c1820583ff0ceb7c500311bd7d181e64af73
formidablae <81068781+formidablae@users.noreply.github.com>
2022-01-21 01:18:04 +0100

    Solved Arcade / Python / 05 Complexity of Comprehension / 30 Create Spiral Matrix

---
+++ Arcade/Python/05_Complexity_of_Comprehension/30_Create_Spiral_Matrix/main.py
@@ -0,0 +1,17 @@
+def solution(n):
+    dirs = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+    curDir = 0
+    curPos = (n - 1, n - 1)
+    res = [[0] * n for _ in range(n)]
+
+    for i in range(1, n * n + 1):
+        res[curPos[0]][curPos[1]] = i
+        nextPos = curPos[0] + dirs[curDir][0], curPos[1] + dirs[curDir][1]
+        if not (0 <= nextPos[0] < n and
+                0 <= nextPos[1] < n and
+                res[nextPos[0]][nextPos[1]] == 0):
+            curDir = (curDir + 1) % 4
+            nextPos = curPos[0] + dirs[curDir][0], curPos[1] + dirs[curDir][1]
+        curPos = nextPos
+
+    return res
